01dc1dd8ad02f7b2d9dfdd701bbfed70412cba79
Diego Rocha <diego@diegorocha.com.br>
2020-02-24 17:08:19 +0000

    Update django and djangorestframework

--- short_url/urls.py
+++ short_url/urls.py
@@ -3,8 +3,10 @@ from short_url.api import api_router
 from django.conf.urls import url, include
 
 
+app_name = 'short_url'
+
 urlpatterns = [
-    url(r'^api/', include(api_router.urls, namespace='api')),
+    url(r'^api/', include((api_router.urls, 'api'), namespace='api')),
     url(r'(?P<alias>.*)', views.ShortUrlView.as_view(), name='short_url'),
 ]
 
